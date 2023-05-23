22b306732f7c7bf147434240d1c1d7ac0c76a934
Kivurawnuru <kabrtondra@gmail.com>
2023-05-23 11:17:01 +0200

    Continuation in UpgradeMenu

--- Shapes and Vidget/src/vyber/UpgradeItemForce.java
+++ Shapes and Vidget/src/vyber/UpgradeItemForce.java
@@ -4,7 +4,7 @@ import postava.SeznamHrdinu;
 
 import java.util.Scanner;
 
-public class UpgradeItemForce extends UpgradeItem{
+public class UpgradeItemForce extends UpgradeItem {
     public UpgradeItemForce(String description, SeznamHrdinu hrdina) {
         super(description, hrdina);
     }
@@ -16,22 +16,24 @@ public class UpgradeItemForce extends UpgradeItem{
 
 
         System.out.println("Aktuálně máš " + skillpointy + " volných skillpointů.");
-        if(skillpointy <= 0){
+        if (skillpointy <= 0) {
             System.out.println("Aktuáně nemáš žádné skillpointy, které bys mohl použít.");
             return false;
-            }else{
+        } else {
             int sprOdpoved = 0;
-            while(sprOdpoved == 0){
-            System.out.println("Kolikrát ji chceš vylepšit.");
+            while (sprOdpoved == 0) {
+                System.out.println("Kolikrát ji chceš vylepšit.");
                 int kolikrat = sc.nextInt();
-            if(kolikrat > 0 && kolikrat <= skillpointy){
-                for(int i = 0; i < kolikrat; i++){
-                    double zviseniSily = hrdina.getHrdinove().get(0).getSila();
-                    zviseniSily = zviseniSily + 0.5;
-                    hrdina.getHrdinove().get(0).setSila(zviseniSily);
+                if (kolikrat > 0 && kolikrat <= skillpointy) {
+                    for (int i = 0; i < kolikrat; i++) {
+                        double zviseniSily = hrdina.getHrdinove().get(0).getSila();
+                        zviseniSily = zviseniSily + 0.5;
+                        hrdina.getHrdinove().get(0).setSila(zviseniSily);
+                        skillpointy--;
+                        hrdina.getHrdinove().get(0).setSkillpoint(skillpointy);
+                    }
+                    sprOdpoved++;
                 }
-                sprOdpoved++;
-            }
 
             }
 
